7c87301a6cfc999a01fa0927094a7f9145cca180
nikhilkalige <nikhilkalige@gmail.com>
2016-07-21 16:19:42 -0700

    Add tests for api protection

--- tests/test_cache_stats.py
+++ tests/test_cache_stats.py
@@ -1,7 +1,8 @@
 import pytest
 import os
 from flask import Flask
-from flask_cache_stats.stats import Cache
+from flask_cache_stats.stats import Cache, CacheStats
+from flask_login import LoginManager, UserMixin, login_user
 
 
 @pytest.yield_fixture()
@@ -153,3 +154,102 @@ def test_delete_many(cache):
     assert data.hot is True
 
     assert len(cache._log.keys()) == 2
+
+
+class User(UserMixin):
+    def __init__(self, name, id, active=True):
+        self.id = id
+        self.name = name
+        self.active = active
+
+    def get_id(self):
+        return self.id
+
+    @property
+    def is_active(self):
+        return self.active
+
+
+valid = User(u'valid', 1)
+invalid = User(u'invalid', 2, False)
+
+USERS = [valid, invalid]
+
+
+@pytest.yield_fixture()
+def app_login():
+    app = Flask(__name__)
+    app.debug = True
+    app.config['SECRET_KEY'] = 'deterministic'
+    app.config['SESSION_PROTECTION'] = None
+    app.config['REMEMBER_COOKIE_NAME'] = 'remember'
+    app.config['CACHE_TYPE'] = 'simple'
+
+    cache = Cache(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager._login_disabled = False
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return USERS[int(user_id)]
+
+    yield app, cache
+
+
+def test_api_unprotected(app_login):
+    app, cache = app_login
+    stats_bp = CacheStats(cache, enable_clear_api=True, protect_api=False)
+    app.register_blueprint(stats_bp)
+    cache.set('hi', 'hello')
+
+    with app.test_client() as c:
+        result = c.delete('cache_stats/asdf')
+        assert result.status_code == 404
+
+        result = c.delete('cache_stats/hi')
+        assert result.status_code == 200
+
+
+def test_api_prefix(app_login):
+    app, cache = app_login
+    stats_bp = CacheStats(cache, url_prefix="/test", enable_clear_api=True,
+                          protect_api=False)
+    app.register_blueprint(stats_bp)
+    cache.set('hi', 'hello')
+
+    with app.test_client() as c:
+        result = c.delete('test/hi')
+        assert result.status_code == 200
+
+
+def test_api_protected(app_login):
+    app, cache = app_login
+    stats_bp = CacheStats(cache, enable_clear_api=True,
+                          protect_api=True)
+    app.register_blueprint(stats_bp)
+
+    @app.route('/login-valid')
+    def login_valid():
+        return str(login_user(valid))
+
+    @app.route('/login-invalid')
+    def login_invalid():
+        return str(login_user(invalid))
+
+    with app.test_request_context():
+        with app.test_client() as c:
+            c.get('/login-valid')
+            cache.set('hi', 'hello')
+            result = c.delete('cache_stats/hi')
+            assert result.status_code == 200
+
+    with app.test_request_context():
+        with app.test_client() as c:
+            cache.set('hi', 'hello')
+            result = c.delete('cache_stats/hi')
+            assert result.status_code == 401
+
+            c.get('login-invalid')
+            result = c.delete('cache_stats/hi')
+            assert result.status_code == 401
